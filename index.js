c50d34246e05751a1b89f96f0bef36c032a7ff65
Scott Phillips <scottphillips1451@sbcglobal.net>
2018-06-13 15:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function happyHolidays(){
   return "Happy Holidays!"
 }
 
+function happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
+
